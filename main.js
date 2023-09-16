5c12877ead01980796229cc4738fdf563330427e
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-09-16 15:36:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function initialize() {
     const audio = document.createElement("audio");
     audio.src = "./beepSound.mp3";
 
-    const h2 = document.querySelector("h2");
-    const h3 = document.querySelector('h3');
+    const h2DisplayTime = document.querySelector("h2");
+    const h3TimeEnded = document.querySelector('h3');
     let seconds;
     let interval;
     const hiddenContainer = document.querySelector(".container");
@@ -11,11 +11,11 @@ function initialize() {
     const customBtn = document.getElementById("custom-time");
     customBtn.addEventListener("click", () => {
         if (hiddenContainer.style.display === "none") { // Showing the hidden div
-            if (h2.textContent != "") { // The timer is standing in the way
+            if (h2DisplayTime.textContent != "") { // The timer is standing in the way
                 clearInterval(interval);
-                h2.textContent = "";
-                h2.style.boxShadow = "none";
-                h3.textContent = "";
+                h2DisplayTime.textContent = "";
+                h2DisplayTime.style.boxShadow = "none";
+                h3TimeEnded.textContent = "";
             }
             hiddenContainer.style.display = "block";
             customBtn.textContent = "Start";
@@ -37,7 +37,8 @@ function initialize() {
     liElements.forEach(li => {
         li.addEventListener("click", () => {
             seconds = Number(li.textContent) * 60;
-            h3.textContent = "";
+            h3TimeEnded.textContent = "";
+            hiddenContainer.style.display = "none";
             clearInterval(interval); // Clearing in case there was another before that
             interval = setInterval(displayRemainingTime, 1000);
         })
@@ -46,7 +47,7 @@ function initialize() {
     function displayRemainingTime() {
         if (seconds < 0) {
             clearInterval(interval);
-            h3.textContent = "Time's up!";
+            h3TimeEnded.textContent = "Time's up!";
             audio.play();
             return;
         }
@@ -64,17 +65,17 @@ function initialize() {
         }
 
         if ((currentHours != 0 && currentMinutes === 0 && currentSeconds === 0) || (currentHours === 0 && currentMinutes === 0 && (currentSeconds === 30 || currentSeconds === 10 || currentSeconds < 6))) {
-            h2.style.color = "#990000";
-            h2.style.boxShadow = "0px 2px 5px 6px rgba(153,0,0,1)"
+            h2DisplayTime.style.color = "#990000";
+            h2DisplayTime.style.boxShadow = "0px 2px 5px 6px rgba(153,0,0,1)";
         } else {
-            h2.style.color = 'var(--white-color)';
-            h2.style.boxShadow = "none";
+            h2DisplayTime.style.color = 'var(--white-color)';
+            h2DisplayTime.style.boxShadow = "none";
         }
 
         if (currentHours > 0) {
-            h2.innerText = `${`${currentHours}`.length === 2 ? currentHours : `0${currentHours}`}:${`${currentMinutes}`.length === 2 ? currentMinutes : `0${currentMinutes}`}:${`${currentSeconds}`.length === 2 ? currentSeconds : `0${currentSeconds}`}`
+            h2DisplayTime.innerText = `${`${currentHours}`.length === 2 ? currentHours : `0${currentHours}`}:${`${currentMinutes}`.length === 2 ? currentMinutes : `0${currentMinutes}`}:${`${currentSeconds}`.length === 2 ? currentSeconds : `0${currentSeconds}`}`
         } else {
-            h2.innerText = `${`${currentMinutes}`.length === 2 ? currentMinutes : `0${currentMinutes}`}:${`${currentSeconds}`.length === 2 ? currentSeconds : `0${currentSeconds}`}`;
+            h2DisplayTime.innerText = `${`${currentMinutes}`.length === 2 ? currentMinutes : `0${currentMinutes}`}:${`${currentSeconds}`.length === 2 ? currentSeconds : `0${currentSeconds}`}`;
         }
         seconds--;
       }
@@ -82,11 +83,12 @@ function initialize() {
     const changeTheme = document.querySelector("#change-theme");
     changeTheme.addEventListener("click", () => {
         if (getComputedStyle(document.querySelector("body")).backgroundImage.includes('url("https://wallpapers.com/images/featured/simple-clean-8g9017acqfddycrl.jpg")')) {
-        document.querySelector("body").style.backgroundImage = 'url("https://i.pinimg.com/originals/99/90/56/999056cafa9423cf4166ee5be451611f.jpg")';
-        document.documentElement.style.setProperty("--white-color", "#3e3e3e");
-        document.documentElement.style.setProperty("--element-background-color", "#efc79f");
-        document.documentElement.style.setProperty("--element-hover-background-color", "#f1cda9");
-        document.documentElement.style.setProperty("--custom-time-button-hover", "#f2d2b2");
+            document.querySelector("body").style.backgroundImage = 'url("https://i.pinimg.com/originals/99/90/56/999056cafa9423cf4166ee5be451611f.jpg")';
+            document.documentElement.style.setProperty("--white-color", "#3e3e3e");
+            document.documentElement.style.setProperty("--element-background-color", "#efc79f");
+            document.documentElement.style.setProperty("--element-hover-background-color", "#f1cda9");
+            document.documentElement.style.setProperty("--custom-time-button-hover", "#f2d2b2");
+            document.documentElement.style.setProperty("--heading-color", "-webkit-linear-gradient(135deg, #008F7A, #C34A36)");
       
         } else {
             document.querySelector("body").style.backgroundImage = 'url("https://wallpapers.com/images/featured/simple-clean-8g9017acqfddycrl.jpg")';
@@ -94,8 +96,8 @@ function initialize() {
             document.documentElement.style.setProperty("--element-background-color", "#55c6b1");
             document.documentElement.style.setProperty("--element-hover-background-color", "#449e8e");
             document.documentElement.style.setProperty("--custom-time-button-hover", "#2a7d6d");
+            document.documentElement.style.setProperty("--heading-color", "-webkit-linear-gradient(135deg, rgb(225, 73, 13), rgb(13, 201, 0))");
         }
     });
     
 }
-
